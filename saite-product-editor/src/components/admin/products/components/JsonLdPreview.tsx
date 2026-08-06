1deab75d67a1666e4ed8d730eeb7a7b708bef3fd
javad <parssystem1@gmail.com>
2026-08-06 19:41:58 +0330

    Delete saite-product-editor directory

--- saite-product-editor/src/components/admin/products/components/JsonLdPreview.tsx
+++
@@ -1,3 +0,0 @@
-export function JsonLdPreview({ value }: { value: unknown }) {
-  return <pre dir="ltr" className="max-h-96 overflow-auto rounded-[var(--radius-md)] border border-[hsl(var(--border))] bg-[hsl(var(--surface-0))] p-4 text-left text-xs leading-6 text-[hsl(var(--muted-foreground))]">{JSON.stringify(value, null, 2)}</pre>;
-}
